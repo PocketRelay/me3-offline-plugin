788e04d4838d7a09ebbecb42a3755a49806f530b
Jacobtread <jacobtread@gmail.com>
2024-06-05 21:12:46 +1200

    refactor: clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,53 @@
 #![warn(unused_crate_dependencies)]
 
-use windows_sys::Win32::System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH};
-
-pub mod hooks;
-
-/// Handles the plugin being attached to the game
-fn attach() {
-    // Debug allocates a console window to display output
-    #[cfg(debug_assertions)]
-    {
-        unsafe { windows_sys::Win32::System::Console::AllocConsole() };
-    }
-
-    // Apply the host lookup hook
-    unsafe { hooks::hook_internet_connected() };
-}
-
-/// Handles the plugin being detached from the game, this handles
-/// cleaning up any extra allocated resources
-fn detach() {
-    // Debug console must be freed on detach
-    #[cfg(debug_assertions)]
-    {
-        unsafe {
-            windows_sys::Win32::System::Console::FreeConsole();
-        }
-    }
-}
+use retour::GenericDetour;
+use windows_sys::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
+use windows_sys::Win32::{
+    Foundation::{BOOL, TRUE},
+    Networking::WinInet::{
+        InternetGetConnectedState, INTERNET_CONNECTION, INTERNET_CONNECTION_LAN,
+    },
+};
 
 /// Windows DLL entrypoint for the plugin
 #[no_mangle]
 extern "stdcall" fn DllMain(_hmodule: isize, reason: u32, _: *mut ()) -> bool {
-    match reason {
-        // Handle attaching
-        DLL_PROCESS_ATTACH => attach(),
-        // Handle detaching
-        DLL_PROCESS_DETACH => detach(),
-        _ => {}
+    if let DLL_PROCESS_ATTACH = reason {
+        hook_internet_connected()
     }
 
     true
+}
+
+type InternetGetConnectedStateFn = unsafe extern "system" fn(*mut INTERNET_CONNECTION, u32) -> i32;
+
+/// Applies a hook that tricks the program into thinking it always has
+/// an active internet connection. (Allows playing offline)
+pub fn hook_internet_connected() {
+    unsafe {
+        GenericDetour::new(
+            InternetGetConnectedState as InternetGetConnectedStateFn,
+            fake_internet_get_state,
+        )
+        .expect("Failed to create detour")
+        .enable()
+        .expect("Failed to enable detour")
+    };
+}
+
+/// Offline check that always returns TRUE
+///
+/// ## Safety
+///
+/// Doesn't perform any unsafe actions, just must be marked as unsafe to
+/// be used as an extern fn
+#[no_mangle]
+pub unsafe extern "system" fn fake_internet_get_state(
+    lpdw_flags: *mut INTERNET_CONNECTION,
+    _dw_reserved: u32,
+) -> BOOL {
+    if !lpdw_flags.is_null() {
+        *lpdw_flags = INTERNET_CONNECTION_LAN;
+    }
+    TRUE
 }
